0b336af5a602bd74a6533890ac3251aac40efec8
minyu <minyu7374@gmail.com>
2021-11-03 16:38:46 +0800

    只要是 mac 就执行 exec-path-from-shell-arguments

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -203,10 +203,10 @@
   )
 
 ;;;; mac 下环境变量
-;; Mac GUI 只有基础的环境变量集，需加载shell环境变量
+;; Mac(GUI) 只有基础的环境变量集，需加载shell环境变量
 ;;(when (memq window-system '(mac ns x))
 (use-package! exec-path-from-shell
-  :if (and IS-MAC (display-graphic-p))
+  :if IS-MAC
   :custom
   (exec-path-from-shell-arguments '("-l"))
   (exec-path-from-shell-variables '("PATH" "MANPATH" "GOPATH" "PYTHONPATH" "C_INCLUDE_PATH"))
