e5472409e42b9e30d3bbf314cd2951eaa9536149
minyu <minyu@outlook.com>
2024-12-18 16:52:51 +0800

    proxy开启函数调整成可交互输入 ip 和 端口

--- doom.d/lisp/proxy.el
+++ doom.d/lisp/proxy.el
@@ -7,22 +7,32 @@
 (defvar proxy-status nil "Proxy Status (t/nil).")
 
 (defun proxy-on()
+  "Socks5 Proxy On."
   (interactive)
 
-  (setq url-gateway-method 'socks)
-  (setq socks-server '("Default server" "127.0.0.1" 17890 5))
-  (setq proxy-status t)
+  (let* ((default-ip "127.0.0.1")
+         (default-port 7890)
+         (ip (read-string (format "proxy server IP (default %s): " default-ip) nil nil default-ip))
+         (port (string-to-number (read-string (format "proxy server port (default %d): " default-port)
+                                              nil nil (number-to-string default-port)))))
+    (setq url-gateway-method 'socks)
+    (setq socks-server `("Default server" ,ip ,port 5))
+    (setq proxy-status t)
+    (message "open proxy server: %s:%d" ip port))
   )
 
 (defun proxy-off()
+  "Socks5 Proxy Off."
   (interactive)
 
   (setq url-gateway-method 'native)
   (setq socks-server nil)
   (setq proxy-status nil)
+  (message "off proxy server")
   )
 
 (defun proxy-toggle()
+  "Socks5 Proxy Toggle."
   (interactive)
 
   (if proxy-status
