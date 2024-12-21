83f0773c36de305b71ec8673ba0ceec4277eee44
minyu <minyu@outlook.com>
2024-12-21 22:05:30 +0800

    添加GPT4All

--- doom.d/lisp/ai.el
+++ doom.d/lisp/ai.el
@@ -20,28 +20,49 @@ DEFAULT-HOST is the fallback value if ENV-VAR is not set or empty."
 PASS-VAR is the key for the token stored in GNU Pass.
 ENV-VAR is the environment variable to use as a fallback if PASS-VAR is empty."
   (let ((token (string-trim (shell-command-to-string (format "pass %s" pass-var)))))
-    
-(if (or (not token) (string-empty-p token))
+    (if (or (not token) (string-empty-p token))
         (getenv env-var) token)))
 
 (use-package! gptel
   :config
+  (setq gptel-max-tokens 4096)
   (setq gptel-api-key (get-api-token "openai/token" "OPENAI_API_KEY"))
 
-  (let ((chatanywhere-host (get-api-host "CHATANYWHERE_IP_HOST" "api.chatanywhere.tech"))
-        (chatanywhere-token (get-api-token "chatanywhere/token" "CHATANYWHERE_IP_KEY")))
+  (let ((chatanywhere-host (get-api-host "CHATANYWHERE_API_HOST" "api.chatanywhere.tech"))
+        (chatanywhere-token (get-api-token "chatanywhere/token" "CHATANYWHERE_API_KEY")))
     (defvar gptel--chatanywhere
       (gptel-make-openai "ChatAnyWhere"
         :host chatanywhere-host
         :key chatanywhere-token
-        :endpoint "/v1/chat/completions"
-        :models '("gpt-4" "gpt-4o" "gpt-4o-mini")
-        :stream t)))
+        ;; :header `(("Authorization" . ,(concat "Bearer " chatanywhere-token)))
+        ;; :endpoint "/v1/chat/completions"
+        :stream t
+        :models '("gpt-4" "gpt-4o" "gpt-4o-mini"))))
 
-  (let ((gemini-token (get-api-token "gemini/token" "GEMINI_IP_KEY")))
+  (let ((gemini-token (get-api-token "gemini/token" "GEMINI_API_KEY")))
     (defvar gptel--gemini (gptel-make-gemini "Gemini" :key gemini-token :stream t)))
 
-  ;; TODO 定义其他大模型后端 ...
+  ;; Gentoo-GMK 本地部署，直接(或ssh端口转发)进行请求。
+  (defvar gptel--gpt4all
+    (gptel-make-gpt4all "GPT4All"
+      :protocol "http"
+      :host "localhost:4891"
+      :stream nil
+      :models '(qwen2.5-coder-7b-instruct-q4_0.gguf
+                Meta-Llama-3-8B-Instruct-Q4_0.gguf
+                Llama-3.2-3B-Instruct-Q4_0.gguf)))
+
+  ;; 通过nginx反向代理转发，nginx中做简单的authorization
+  (let ((minyuchat-host (get-api-host "MINYUCHAT_API_HOST" "chat.wminyu.top:433"))
+        (minyuchat-token (get-api-token "minyuchat/token" "MINYUCHAT_API_KEY")))
+    (defvar gptel--minyuchat
+      (gptel-make-gpt4all "Minyu"
+        :host minyuchat-host
+        :key minyuchat-token
+        :stream nil
+        :models '(qwen2.5-coder-7b-instruct-q4_0.gguf
+                  Meta-Llama-3-8B-Instruct-Q4_0.gguf
+                  Llama-3.2-3B-Instruct-Q4_0.gguf))))
 
   ;; 默认后端
   (setq-default gptel-backend gptel--chatanywhere)
