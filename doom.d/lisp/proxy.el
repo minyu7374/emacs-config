ad31aa8cb8167b239266beeac9d8c9a2f372d5ce
minyu <minyu@outlook.com>
2025-01-08 11:02:45 +0800

    fix: proxy 配置

--- doom.d/lisp/proxy.el
+++ doom.d/lisp/proxy.el
@@ -5,53 +5,51 @@
 ;;; code:
 
 (defvar proxy-status nil "Proxy Status (t/nil).")
-(defvar default-proxy-ip "127.0.0.1" "Default Socks5 Proxy IP.")
-(defvar default-proxy-port 7890 "Default Socks5 Proxy Port.")
+(defvar default-proxy-ip "127.0.0.1" "Default Proxy Server IP.")
+(defvar default-proxy-port 7890 "Default Proxy Server Port.")
 
 (defun proxy-on()
-  "Socks5 Proxy On with Custom IP and Port."
+  "Proxy On with Custom IP and Port."
   (interactive)
 
-  (let ((ip (read-string (format "proxy server IP (default %s): " default-proxy-ip) nil nil default-proxy-ip))
-        (port (string-to-number (read-string (format "proxy server port (default %d): " default-proxy-port)
-                                             nil nil (number-to-string default-proxy-port)))))
-    (_proxy-on ip port))
+  (let* ((ip (read-string (format "proxy server IP (default %s): " default-proxy-ip) nil nil default-proxy-ip))
+         (port (string-to-number (read-string (format "proxy server port (default %d): " default-proxy-port)
+                                              nil nil (number-to-string default-proxy-port))))
+         ;;(use-socks (y-or-n-p "Use socks proxy(y/n)?")))
+         (use-socks-str (read-string "Use SOCKS proxy(y/N)? " nil nil "N"))
+         (use-socks (string-equal (downcase use-socks-str) "y")))
+
+    (_proxy-on ip port use-socks))
   )
 
-(defun _proxy-on(ip port)
-  (setq url-gateway-method 'socks)
-  (setq socks-server `("Default server" ,ip ,port 5))
-  ;; (let ((proxy-server (format "socks5://%s:%d" ip port)))
-  (let ((proxy-server (format "socks5h://%s:%d" ip port)))
-       (setq url-proxy-services
-             `(("no_proxy" . "^\\(localhost\\|127\\..*\\|10\\..*\\|192\\.168\\..*\\)")  ; elisp正则 \\( \\| \\. 即普通正则的( | \.
-               ("http" . ,proxy-server)
-               ("https" . ,proxy-server)))
-       (setenv "http_proxy" proxy-server)
-       (setenv "https_proxy" proxy-server)
-       (setenv "ALL_PROXY" proxy-server))
-       (setenv "NO_PROXY" "localhost,127.0.0.0/8,10.0.0.0/8,192.168.0.0/16")
+(defun _proxy-on(ip port use-socks)
+  (if use-socks
+      (progn
+        (setq url-gateway-method 'socks)
+        (setq socks-server `("Default server" ,ip ,port 5))
+        (message "set socks proxy server: %s:%d" ip port))
+    (progn
+      (setq url-gateway-method 'native)
+      (setq url-proxy-services
+            `(("no_proxy" . "^\\(localhost\\|127\\..*\\|10\\..*\\|192\\.168\\..*\\)")  ; elisp正则 \\( \\| \\. 即普通正则的( | \.
+              ("http" . ,(format "%s:%d" ip port))))
+      (message "set http proxy server: %s:%d" ip port)))
   (setq proxy-status t)
-  (message "set proxy server: %s:%d" ip port)
   )
 
 (defun proxy-off()
-  "Socks5 Proxy Off."
+  "Proxy Off."
   (interactive)
 
   (setq url-gateway-method 'native)
   (setq socks-server nil)
   (setq url-proxy-services nil)
-  (setenv "http_proxy" nil)
-  (setenv "https_proxy" nil)
-  (setenv "ALL_PROXY" nil)
-  (setenv "NO_PROXY" nil)
   (setq proxy-status nil)
   (message "off proxy server")
   )
 
 (defun proxy-toggle()
-  "Socks5 Proxy Toggle."
+  "Proxy Toggle."
   (interactive)
 
   (if proxy-status
@@ -60,7 +58,7 @@
   )
 
 ;; 现在使用chatgpt，启动emacs就按默认值开启代理
-(_proxy-on default-proxy-ip default-proxy-port)
+(_proxy-on default-proxy-ip default-proxy-port nil)
 
 (global-set-key (kbd "\C-cyp") 'proxy-toggle)
 
