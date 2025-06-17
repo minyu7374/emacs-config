d631225a60ef9f34bacf4a3e1f85401b1265b85d
minyu <minyu@outlook.com>
2025-06-17 21:04:03 +0800

    添加chatgpt-shell

--- doom.d/lisp/ai.el
+++ doom.d/lisp/ai.el
@@ -8,84 +8,75 @@
 
 ;;; Code:
 
-;; gptel
-(defun get-api-host (env-var default-host)
-  "Retrieve the API host from an environment variable.
+(defun get-llm-api-host (env-var default-host)
+  "Retrieve the llm api host from an environment variable.
 ENV-VAR is the name of the environment variable.
 DEFAULT-HOST is the fallback value if ENV-VAR is not set or empty."
   (let ((host (getenv env-var)))
     (if (or (not host) (string-empty-p host)) default-host host)))
 
-(defun get-api-token (pass-var env-var)
-  "Retrieve the API token from a password manager or an environment variable.
+(defun get-llm-api-token (pass-var env-var)
+  "Retrieve the llm api token from a password manager or an environment variable.
 PASS-VAR is the key for the token stored in GNU Pass.
 ENV-VAR is the environment variable to use as a fallback if PASS-VAR is empty."
   (let ((token (string-trim (shell-command-to-string (format "pass %s" pass-var)))))
     (if (or (not token) (string-empty-p token))
         (getenv env-var) token)))
 
+(defvar llm-api-keys
+  `((openai
+     :token ,(lambda () (get-llm-api-token "openai/token" "OPENAI_API_KEY")))
+    (chatanywhere
+     :host ,(lambda () (get-llm-api-host "CHATANYWHERE_API_HOST" "api.chatanywhere.tech"))
+     :token ,(lambda () (get-llm-api-token "chatanywhere/token" "CHATANYWHERE_API_KEY")))
+    (gemini
+     :token ,(lambda () (get-llm-api-token "gemini/token" "GEMINI_API_KEY")))
+    (minyuchat
+     :host ,(lambda () (get-llm-api-host "MINYUCHAT_API_HOST" "chat.wminyu.top:433"))
+     :ds-host ,(lambda () (get-llm-api-host "MINYUCHAT_DS_API_HOST" "chat.wminyu.top:433/v1"))
+     :token ,(lambda () (get-llm-api-token "deepseek/web-token" "DEEPSEEK_WEB_API_KEY")))))
+
+(defun get-llm-api-key (backend key)
+  "Retrieve property value for a llm api.
+BACKEND is the name of the llm(e.g. gemini, chatanywhere, ...).
+KEY is the property to retrieve (:*host or :token)."
+  (let ((entry (alist-get backend llm-api-keys)))
+    (when-let ((val (plist-get entry key)))
+      (funcall val))))
+
+;; gptel https://github.com/karthink/gptel
 (use-package! gptel
   :config
   (setq gptel-max-tokens 4096)
   (setq gptel-track-media t)
 
-  ;; (setq gptel-api-key (get-api-token "openai/token" "OPENAI_API_KEY"))
+  ;; (setq gptel-api-key (get-llm-api-key 'openai :token))
 
-  ;; (let ((anthropic-token (get-api-token "anthropic/token" "ANTHROPIC_API_KEY")))
-  ;;   (defvar gptel--anthropic (gptel-make-anthropic "Claude" :key anthropic-token :stream t)))
+  (defvar gptel--gemini (gptel-make-gemini "Gemini" :key (get-llm-api-key 'gemini :token) :stream t))
 
-  (let ((gemini-token (get-api-token "gemini/token" "GEMINI_API_KEY")))
-    (defvar gptel--gemini (gptel-make-gemini "Gemini" :key gemini-token :stream t)))
-
-  (let ((chatanywhere-host (get-api-host "CHATANYWHERE_API_HOST" "api.chatanywhere.tech"))
-        (chatanywhere-token (get-api-token "chatanywhere/token" "CHATANYWHERE_API_KEY")))
-    (defvar gptel--chatanywhere
-      (gptel-make-openai "ChatAnyWhere"
-        :host chatanywhere-host
-        :key chatanywhere-token
-        ;; :header `(("Authorization" . ,(concat "Bearer " chatanywhere-token)))
-        ;; :endpoint "/v1/chat/completions"
-        :stream t
-        :models '(gpt-4 gpt-4o gpt-4o-mini))))
+  (defvar gptel--chatanywhere
+    (gptel-make-openai "ChatAnyWhere"
+      :host (get-llm-api-key 'chatanywhere :host)
+      :key (get-llm-api-key 'chatanywhere :token)
+      ;; :header `(("Authorization" . ,(concat "Bearer " chatanywhere-token)))
+      ;; :endpoint "/v1/chat/completions"
+      :stream t
+      :models '(gpt-4 gpt-4o gpt-4o-mini)))
 
   ;; deepseek web页面逆向api部署本地，nginx反向代理
-  (let ((minyuchat-host (get-api-host "MINYUCHAT_API_HOST" "chat.wminyu.top:433"))
-        (minyuchat-token (get-api-token "deepseek/web-token" "DEEPSEEK_WEB_API_KEY")))
-    (defvar gptel--minyuchat
-      (gptel-make-openai "MinyuChat"
-        :host minyuchat-host
-        :key minyuchat-token
-        :stream t
-        :models '(deepseek-chat deepseek-coder deepseek-reasoner))))
-
-  ;; ;; GPT4All Gentoo-GMK 本地部署，直接(或其他设备通过ssh端口转发)进行请求。
-  ;; (defvar gptel--gpt4all
-  ;;   (gptel-make-gpt4all "GPT4All"
-  ;;     :protocol "http"
-  ;;     :host "localhost:4891"
-  ;;     :stream nil
-  ;;     :models '(qwen2.5-coder-7b-instruct-q4_0.gguf
-  ;;               Meta-Llama-3-8B-Instruct-Q4_0.gguf
-  ;;               Llama-3.2-3B-Instruct-Q4_0.gguf)))
-
-  ;; ;; 通过nginx反向代理转发，nginx中做简单的authorization
-  ;; (let ((minyuchat-host (get-api-host "MINYUCHAT_API_HOST" "chat.wminyu.top:433"))
-  ;;       (minyuchat-token (get-api-token "minyuchat/token" "MINYUCHAT_API_KEY")))
-  ;;   (defvar gptel--minyuchat
-  ;;     (gptel-make-gpt4all "Minyu"
-  ;;       :host minyuchat-host
-  ;;       :key minyuchat-token
-  ;;       :stream nil
-  ;;       :models '(qwen2.5-coder-7b-instruct-q4_0.gguf
-  ;;                 Meta-Llama-3-8B-Instruct-Q4_0.gguf
-  ;;                 Llama-3.2-3B-Instruct-Q4_0.gguf))))
+  (defvar gptel--minyuchat
+    (gptel-make-openai "MinyuChat"
+      :host (get-llm-api-key 'minyuchat :host)
+      :key (get-llm-api-key 'minyuchat :token)
+      :stream t
+      :models '(deepseek-chat deepseek-coder deepseek-reasoner)))
 
   ;; 默认后端
   (setq-default gptel-backend gptel--minyuchat)
 
   (global-set-key (kbd "\C-cA") 'gptel)
   (map! :leader
-        (:prefix ("ya" . "AI")
+        (:prefix ("ya" . "gptel")
          :desc "Create new chat buffer" :nv "a" #'gptel
          :desc "Menu for chat preferences" :nv "m" #'gptel-menu
          :desc "Rewrite/refactor selected region" :nv "w" #'gptel-rewrite
@@ -94,6 +85,24 @@ ENV-VAR is the environment variable to use as a fallback if PASS-VAR is empty."
          :desc "Stop active gptel process" :nv "k" #'gptel-abort))
   )
 
+;; chatgpt-shell https://github.com/xenodium/chatgpt-shell
+(use-package! chatgpt-shell
+  :config
+  (setq chatgpt-shell-api-url-base             (format "https://%s" (get-llm-api-key 'chatanywhere :host))
+        chatgpt-shell-openai-key               (get-llm-api-key 'chatanywhere :token)
+        chatgpt-shell-google-key               (get-llm-api-key 'gemini :token)
+        chatgpt-shell-deepseek-api-url-base    (format "https://%s" (get-llm-api-key 'minyuchat :ds-host))
+        chatgpt-shell-deepseek-key             (get-llm-api-key 'minyuchat :token))
+
+  (global-set-key (kbd "\C-cC") 'chatgpt-shell)
+  (map! :leader
+        (:prefix ("yc" . "chatgpt-shell")
+         :desc "Start new Chatgpt-Shell interactive" :nv "c" #'chatgpt-shell
+         :desc "Compose and send prompt from a dedicated buffer" :nv "e" #'chatgpt-shell-prompt-compose
+         :desc "Cancel and close compose buffer" :nv "q" #'chatgpt-shell-prompt-compose-cancel
+         :desc "Chatgpt-Shell swap model" :nv "v" #'chatgpt-shell-swap-model
+         :desc "Chatgpt-Shell swap prompt" :nv "s" #'chatgpt-shell-swap-system-prompt))
+  )
 
 ;; copilot https://github.com/copilot-emacs/copilot.el
 ;; accept completion from copilot and fallback to company
@@ -117,7 +126,8 @@ ENV-VAR is the environment variable to use as a fallback if PASS-VAR is empty."
   (add-to-list 'copilot-indentation-alist '(prog-mode 4))
   (add-to-list 'copilot-indentation-alist '(org-mode 2))
   (add-to-list 'copilot-indentation-alist '(text-mode 2))
-  (add-to-list 'copilot-indentation-alist '(emacs-lisp-mode 2)))
+  (add-to-list 'copilot-indentation-alist '(emacs-lisp-mode 2))
+  )
 
 (after! (evil copilot)
   ;; Define a custom function for handling Copilot or fallback actions
@@ -136,7 +146,8 @@ ENV-VAR is the environment variable to use as a fallback if PASS-VAR is empty."
     (kbd "S-<tab>") (lambda () (interactive) (my/copilot-action-or-default #'copilot-accept-completion-by-word))
     (kbd "S-TAB") (lambda () (interactive) (my/copilot-action-or-default #'copilot-accept-completion-by-word))
     (kbd "C-n") (lambda () (interactive) (my/copilot-action-or-default #'copilot-next-completion))
-    (kbd "C-p") (lambda () (interactive) (my/copilot-action-or-default #'copilot-previous-completion))))
+    (kbd "C-p") (lambda () (interactive) (my/copilot-action-or-default #'copilot-previous-completion)))
+  )
 
 (provide 'ai)
 
