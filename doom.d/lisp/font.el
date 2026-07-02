37cba15720a2e5c93cfba450cf7c856607a82276
minyu <minyu@outlook.com>
2026-07-02 17:13:17 +0800

    macos 字体大小调整

--- doom.d/lisp/font.el
+++ doom.d/lisp/font.el
@@ -3,10 +3,10 @@
 ;;;     字体配置，根据系统和显示器DPI动态调整。
 ;;; Code:
 
-(defvar default-font-size 14 "Default font size for doom theme.")
+(defvar default-font-size 13 "Default font size for doom theme.")
 (defvar big-font-size 18 "Big font size for doom theme.")
-;; 实测现在用的字体，缩放1.25倍后对齐两个英文字母，但是中文字体显得特别大，不太美观，平时还是用1.1吧
-(defvar chinese-font-rescale 1.1 "Chinese font scale ratio for doom theme.")
+;; 调整缩放比例，实现中文字对齐两个英文字母
+(defvar chinese-font-rescale 1.15 "Chinese font scale ratio for doom theme.")
 (defvar blamer-face-height 100 "Height for blamer face.")
 
 (defun my/setup-font-based-on-monitor (&optional frame)
@@ -34,7 +34,7 @@
 (unless (featurep 'font-spec)
   (when (eq system-type 'darwin)
     (setq blamer-face-height 150) ; 仅macOS使用
-    (setq default-font-size 15
+    (setq default-font-size 14
           big-font-size 18))
 
   (when (eq system-type 'gnu/linux)
